8001fdecf0ac89598ca807bb3dfded98a6742864
psyet4 <psyet4@nottingham.ac.uk>
2024-10-26 23:08:57 +0100

    coded setColour function, set title to Reginae

--- slot.gd
+++ slot.gd
@@ -1,5 +1,6 @@
 extends Panel
 
+var styleBox: StyleBoxFlat = get_theme_stylebox("panel").duplicate()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +9,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+func setColour(colour: String) -> void:
+	styleBox.set("bg_color", colour)
+	add_theme_stylebox_override("panel", styleBox)
 	pass
