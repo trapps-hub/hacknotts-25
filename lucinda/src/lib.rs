36147557c236a359c5c3f371204f978c4f2f9743
DTheLegend <me@daudi.dev>
2024-10-26 23:00:41 +0100

    rust tings lol

--- lucinda/src/lib.rs
+++ lucinda/src/lib.rs
@@ -1,14 +1,60 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+mod board;
+
+use std::env::args;
+use godot::classes::{GridContainer, IGridContainer, Panel};
+use godot::classes::resource_loader::ExLoad;
+use godot::prelude::*;
+use godot::sys::InitLevel::Scene;
+use nalgebra::SMatrix;
+use crate::board::BoardBuilder;
+
+struct Lucinda;
+
+#[gdextension]
+unsafe impl ExtensionLibrary for Lucinda {
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(GodotClass)]
+#[class(base = GridContainer)]
+struct LucindaGrid {
+    board: board::ValidBoard,
+    slot_instances: Vec<Gd<Panel>>,
+    base: Base<GridContainer>
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[godot_api]
+impl IGridContainer for LucindaGrid {
+    fn init(base: Base<GridContainer>) -> Self {
+        let board = loop {
+            if let Some(board) = BoardBuilder::new()
+                .place_queens()
+                .flood_fill()
+                .validate_unique() {
+                break board;
+            }
+        };
+
+        Self {
+            board,
+            slot_instances: Vec::new(),
+            base
+        }
+    }
+
+    fn ready(&mut self) {
+        let scene = load("res://slot.tscn");
+
+        self.slot_instances.extend(self.board.iter().map(|x| {
+            let mut y = scene.instantiate_as::<Panel>();
+
+            let args = [x.region.as_color().to_variant()];
+
+            y.call("set_color".into(), &args);
+
+            y
+        }));
+
     }
 }
+
+
