034aeb847eef72861bc92fe47de5d4dd23e0a829
psyet4 <psyet4@nottingham.ac.uk>
2024-10-27 11:44:23 +0000

    seed implementation connected to UI (huge slay)

--- board.gd
+++ board.gd
@@ -4,19 +4,35 @@ var lucinda
 var time = 0
 var updateTime = true
 var winLabel
+var seedField
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	lucinda = get_node("QueensBoard/LucindaGrid")
+	seedField = get_parent().get_node("Footer/HBoxContainer/LineEdit")
+	
 	get_parent()			\
 		.get_parent()	\
-		.get_node("LeftCol/genBoardButton") \
+		.get_node("LeftCol/ButtonContainer/genBoardButton") \
 		.pressed.connect(self._regenerate)
+		
+	get_parent()			\
+		.get_parent()	\
+		.get_node("LeftCol/ButtonContainer/loadBoardButton") \
+		.pressed.connect(self._regenerate_with_seed)
 	winLabel = get_node("WinLabel")
+
+func _regenerate_with_seed() -> void:
+	winLabel.visible = false
+	
+	lucinda.regenerate_with_seed(seedField.text)
+	time = 0
+	updateTime = true
+	pass
 
 func _regenerate() -> void:
 	winLabel.visible = false
-	lucinda.regenerate()
+	seedField.text = lucinda.regenerate_with_random_seed()
 	time = 0
 	updateTime = true
 	
